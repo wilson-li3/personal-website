70129c16a472f742dd78b14b36f04c68dc64ded0
Wilson Li <wyli@uwaterloo.ca>
2026-01-14 16:01:56 -0500

    add images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,15 @@
 import Toolbar from '../components/Toolbar'
 import './Page.css'
+import waterlooCrest from '../images/waterloocrest.png'
+import libertyBell from '../images/libertybell.png'
+import eaglesLogo from '../images/eagles.png'
+import nflLogo from '../images/nfl.png'
+import nbaLogo from '../images/nba.png'
+import soccer from '../images/soccer.png'
+import gym from '../images/gym.png'
+import run from '../images/run.png'
+import music from '../images/music.png'
+import art from '../images/art.png'
 
 function About() {
   const noteDate = new Date(Date.now() - 86400000)
@@ -25,37 +35,37 @@ function About() {
         <ul className="about-list">
           <li>
             – software engineering student at the university of waterloo
-            <img className="inline-image" src="INSERT_WATERLOO_CREST_IMAGE_PATH_HERE" alt="Waterloo crest" />
+            <img className="inline-image" src={waterlooCrest} alt="Waterloo crest" />
           </li>
           <li>– i like building personal projects and learning by doing</li>
           <li>– this site is my notebook for ideas, projects, and things in progress</li>
           <li>– grew up just outside of philadelphia</li>
           <li>
             – big philly pride
-            <img className="inline-image" src="INSERT_LIBERTY_BELL_IMAGE_PATH_HERE" alt="Liberty bell" />
+            <img className="inline-image" src={libertyBell} alt="Liberty bell" />
           </li>
           <li>
             – huge eagles fan: go birds dh
-            <img className="inline-image" src="INSERT_EAGLES_LOGO_IMAGE_PATH_HERE" alt="Eagles logo" />
+            <img className="inline-image" src={eaglesLogo} alt="Eagles logo" />
           </li>
           <li>
             – love watching sports, especially the nfl and nba
-            <img className="inline-image" src="INSERT_NFL_LOGO_IMAGE_PATH_HERE" alt="NFL logo" />
-            <img className="inline-image" src="INSERT_NBA_LOGO_IMAGE_PATH_HERE" alt="NBA logo" />
+            <img className="inline-image" src={nflLogo} alt="NFL logo" />
+            <img className="inline-image" src={nbaLogo} alt="NBA logo" />
           </li>
           <li>
             – outside of school i stay active: soccer + gym
-            <img className="inline-image" src="INSERT_SOCCER_IMAGE_PATH_HERE" alt="Soccer" />
-            <img className="inline-image" src="INSERT_GYM_IMAGE_PATH_HERE" alt="Gym" />
+            <img className="inline-image" src={soccer} alt="Soccer" />
+            <img className="inline-image" src={gym} alt="Gym" />
           </li>
           <li>
             – training for my first half marathon this fall
-            <img className="inline-image" src="INSERT_WEIGHTS_IMAGE_PATH_HERE" alt="Weights" />
+            <img className="inline-image" src={run} alt="Running" />
           </li>
           <li>
             – also into music and art
-            <img className="inline-image" src="INSERT_MUSIC_IMAGE_PATH_HERE" alt="Music" />
-            <img className="inline-image" src="INSERT_ART_IMAGE_PATH_HERE" alt="Art" />
+            <img className="inline-image" src={music} alt="Music" />
+            <img className="inline-image" src={art} alt="Art" />
           </li>
         </ul>
       </div>
