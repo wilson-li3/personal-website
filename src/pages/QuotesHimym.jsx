6e058955ee449d56bf0be0f5a59881d483b3b8f5
Wilson Li <wyli@uwaterloo.ca>
2026-01-15 16:12:07 -0500

    finished himym page

--- src/pages/QuotesHimym.jsx
+++ src/pages/QuotesHimym.jsx
@@ -23,37 +23,37 @@ function QuotesHimym() {
       </div>
       <div className="page-content">
         <h1 className="about-title">quotes from himym</h1>
-        <p className="quotes-description">how i met your mother is one of my favorite shows of all time along side ted lasso and suits. here are some of the quotes that resonated with me</p>
+        <p className="quotes-description">how i met your mother is one of my favorite shows of all time along side ted lasso and suits. here are some of the quotes that resonated with me and what i think</p>
         <img src={himymImage} alt="How I Met Your Mother" className="quotes-image" />
         <div className="quotes-container">
           <ul className="about-list quotes-list">
             <li>
               <div className="quote-text">– "and most importantly, whatever you do in this life, it's not legendary unless your friends are there to see it"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">This is my favorite quote from the entire show. And it’s true. What’s there to celebrate without the people who saw you through ups and downs</div>
             </li>
             <li>
               <div className="quote-text">– "you will be shocked kids, when you discover how easy it is in life to part ways with people forever. that's why when you find someone you wanna keep around, you do something about it"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">It's easy to let people slip away, but when you find someone worth keeping, you have to make an effort to hold on to them.</div>
             </li>
             <li>
               <div className="quote-text">– "you can ask the universe for all the signs you want but ultimately, we only see what we want to see when we're ready to see it"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">dont tunnel vision on what you think you want. be open to the possibilities</div>
             </li>
             <li>
               <div className="quote-text">– "sometimes things have to fall apart to make way for better things"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">failing is part of the process</div>
             </li>
             <li>
               <div className="quote-text">– "kids, you can't cling to the past because no matter how hard you hold on, it's already gone"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">let go of what was and embrace what is</div>
             </li>
             <li>
               <div className="quote-text">– "because sometimes even if you know how something's going to end, that doesn't mean you can't enjoy the ride"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">we're here for a fun time not a long time</div>
             </li>
             <li>
               <div className="quote-text">– "shouldn't we hold out for the person who doesn't just tolerate our little quirks but actually kinda likes them"</div>
-              <div className="quote-thought">your thoughts here</div>
+              <div className="quote-thought">when the right person shows up, you'll know</div>
             </li>
           </ul>
         </div>
