c3704be9a488ca1da96fe2e70cd4ed83c595c0b2
Wilson Li <wyli@uwaterloo.ca>
2026-02-01 21:50:09 -0500

    added landing screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
 import CustomCursor from './components/CustomCursor'
+import MacLanding from './components/MacLanding'
 import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
@@ -11,22 +13,36 @@ import Photos from './pages/Photos'
 import GymRoutine from './pages/GymRoutine'
 import Doodle from './pages/Doodle'
 
+function AppContent() {
+  const location = useLocation()
+  const [hasEnteredNotes, setHasEnteredNotes] = useState(false)
+  const showMacLanding = !hasEnteredNotes
+
+  if (showMacLanding) {
+    return <MacLanding onEnter={() => setHasEnteredNotes(true)} />
+  }
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/quotes-himym" element={<QuotesHimym />} />
+        <Route path="/photos" element={<Photos />} />
+        <Route path="/gym-routine" element={<GymRoutine />} />
+        <Route path="/doodle" element={<Doodle />} />
+      </Routes>
+    </Layout>
+  )
+}
+
 function App() {
   return (
     <BrowserRouter>
       <CustomCursor />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/quotes-himym" element={<QuotesHimym />} />
-          <Route path="/photos" element={<Photos />} />
-          <Route path="/gym-routine" element={<GymRoutine />} />
-          <Route path="/doodle" element={<Doodle />} />
-        </Routes>
-      </Layout>
+      <AppContent />
     </BrowserRouter>
   )
 }
